ba09ba47fea36f25aadc95fe9b097b513484082d
Melania Ion <melaniaion24@gmail.com>
2024-05-11 18:44:35 +0300

    update

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 contract Payment is Ownable {
 
     mapping(address => uint) public votesPaid;  // Tracks the number of paid votes per voter
